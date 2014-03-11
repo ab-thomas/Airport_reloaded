a3f14e65a426b1eb724a5acc520eaeced5e785db
Abraham Thomas <dysruptmedia@gmail.com>
2014-03-11 22:19:50 +0000

    refactored plane_spec.rb

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,54 +2,44 @@ require './lib/plane.rb'
 
   describe 'Plane' do
 
+    let(:plane) { Plane.new("Plane1") }
+
   context 'at initialization' do 
 
     it 'should have a name' do
-      plane = Plane.new("Plane1")
       expect(plane.name).to eq("Plane1")
     end
 
     it 'should be flying' do
-      plane = Plane.new("Plane1")
       expect(plane).to be_flying
     end
 
   end
   
-
   context 'at landing and takeoff' do
   
     it 'should be able to land' do
-      plane = Plane.new("Plane1")
       plane.land
       expect(plane).not_to be_flying
     end
 
     it 'should be able to takeoff' do
-      plane = Plane.new("Plane1")
       plane.land
       plane.take_off
       expect(plane).to be_flying
     end
 
     it 'should not be able to land after landing' do
-      plane = Plane.new("Plane1")
       plane.land
-      expect(plane).not_to be_flying
       expect{plane.land}.to raise_error(RuntimeError)
     end
 
      it 'should not be able to takeoff after taking off' do
-      plane = Plane.new("Plane1")
       plane.land
-      expect(plane).not_to be_flying
       plane.take_off
-      expect(plane).to be_flying
       expect{plane.take_off}.to raise_error(RuntimeError)
     end
 
   end
- 
-
-
+  
   end
