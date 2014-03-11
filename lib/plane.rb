06deaf518db7db2152cd70b04d45f78143071bdc
Abraham Thomas <dysruptmedia@gmail.com>
2014-03-11 22:15:09 +0000

    implented at landing and takeoff context

--- lib/plane.rb
+++ lib/plane.rb
@@ -11,6 +11,16 @@ class Plane
     @flying 
   end
 
+  def land
+    raise "You have already landed!" if @flying == false
+    @flying = false
+  end
+
+  def take_off
+    raise "You have already taken off!" if @flying == true
+    @flying = true
+  end
+
   
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -13,12 +13,43 @@ require './lib/plane.rb'
       plane = Plane.new("Plane1")
       expect(plane).to be_flying
     end
-    
+
   end
+  
+
+  context 'at landing and takeoff' do
+  
+    it 'should be able to land' do
+      plane = Plane.new("Plane1")
+      plane.land
+      expect(plane).not_to be_flying
+    end
+
+    it 'should be able to takeoff' do
+      plane = Plane.new("Plane1")
+      plane.land
+      plane.take_off
+      expect(plane).to be_flying
+    end
+
+    it 'should not be able to land after landing' do
+      plane = Plane.new("Plane1")
+      plane.land
+      expect(plane).not_to be_flying
+      expect{plane.land}.to raise_error(RuntimeError)
+    end
+
+     it 'should not be able to takeoff after taking off' do
+      plane = Plane.new("Plane1")
+      plane.land
+      expect(plane).not_to be_flying
+      plane.take_off
+      expect(plane).to be_flying
+      expect{plane.take_off}.to raise_error(RuntimeError)
+    end
+
   end
  
 
 
-
-
-  
+  end
